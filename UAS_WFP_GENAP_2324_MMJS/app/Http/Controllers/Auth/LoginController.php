d719cb4b56ebf37620131ee691d98aff591d909f
melissaveronica <s160821004@student.ubaya.ac.id>
2024-06-12 17:28:25 +0700

    1

--- UAS_WFP_GENAP_2324_MMJS/app/Http/Controllers/Auth/LoginController.php
+++ UAS_WFP_GENAP_2324_MMJS/app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/type';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
--- UAS_WFP_GENAP_2324_MMJS/app/Http/Controllers/Auth/RegisterController.php
+++ UAS_WFP_GENAP_2324_MMJS/app/Http/Controllers/Auth/RegisterController.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
@@ -29,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/type';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
@@ -52,7 +51,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed']
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
 
@@ -68,7 +67,6 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role'=>$data['role']
         ]);
     }
 }
